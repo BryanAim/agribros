80d625ea806804038bd870c426919e8bb4744e14
Isale <isale.bryan@gmail.com>
2021-08-19 11:47:01 +0300

    register page

---
+++ register.php
@@ -0,0 +1,103 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>AgriBros</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href="bootstrap\css\bootstrap.min.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="bootstrap\js\bootstrap.min.js"></script>
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="login.css"/>
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<link rel="stylesheet" href="indexfooter.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body>
+
+  <?php
+		require 'menu.php';
+	?>
+  <form class="modal-content animate" action="Login/signUp.php" method='POST'>
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+    </div>
+    <div class="container">
+        <section>
+							<h3>SignUp</h3>
+							<form method="post" action="Login/signUp.php">
+								<center>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="name" id="name" value="" placeholder="Name" required/>
+									</div>
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="uname" id="uname" value="" placeholder="UserName" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+										<input type="text" name="mobile" id="mobile" value="" placeholder="Mobile Number" required/>
+									</div>
+
+									<div class="3u 12u$(xsmall)">
+										<input type="email" name="email" id="email" value="" placeholder="Email" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(xsmall)">
+			                            <input type="password" name="password" id="password" value="" placeholder="Password" required/>
+			                        </div>
+			                        <div class="3u 12u$(xsmall)">
+			                            <input type="password" name="pass" id="pass" value="" placeholder="Retype Password" required/>
+			                        </div>
+								</div>
+								<div class="row uniform">
+									<div class="6u 12u$(xsmall)">
+										<input type="text" name="addr" id="addr" value="" placeholder="Address" style="width:80%" required/>
+									</div>
+								</div>
+								<div class="row uniform">
+									<p>
+			                            <b>Category : </b>
+			                        </p>
+			                        <div class="3u 12u$(small)">
+				                            <input type="radio" id="farmer" name="category" value="1">
+				                            <label for="farmer">Farmer</label>
+				                        </div>
+				                        <div class="3u 12u$(small)">
+				                            <input type="radio" id="buyer" name="category" value="0">
+				                            <label for="buyer">Buyer</label>
+				                        </div>
+
+															
+								</div>
+								<div class="row uniform">
+									<div class="3u 12u$(small)">
+										<input type="submit" value="Submit" name="submit" class="special" /></li>
+									</div>
+									<div class="3u 12u$(small)">
+										<input type="reset" value="Reset" name="reset"/></li>
+									</div>
+								</div>
+							</center>
+							</form>
+						</section>
+            </div>
+          </div>
+    </form>
+
+  </body>
+</html>
